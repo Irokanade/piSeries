e909bedb2d7dea35e997c5b20027366cdb965e47
Irokanade <michaeljs.leong@gmail.com>
2022-10-23 14:25:57 +0800

    Initial Commit

---
+++ piSeries/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  piSeries
+//
+//  Created by Michael Leong on 23/10/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
